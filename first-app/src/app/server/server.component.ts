d3bd260fa519119cfe825b4a06ab54747d145313
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-18 12:11:53 +0100

    first component s2l17

--- first-app/src/app/server/server.component.ts
+++ first-app/src/app/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector:'app-server',
-  templateUrl:'./server.components.html'
+  templateUrl:'./server.component.html'
 })
 export class ServerComponent {
 
