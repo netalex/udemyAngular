2756c0cd671736d7712ba3c6186d864573254ecf
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-01-27 08:19:59 +0100

    39. basics-assignment-3-start 2

---
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showPwd:boolean=false;
+  log=[];
+
+  setTimeStamp(){
+   let now = new Date();
+   return now.getHours().toString()+':'+now.getMinutes().toString()+':'+now.getSeconds().toString()+'-'+now.getDay().toString()+'/'+now.getMonth()+'/'+now.getFullYear().toString();
+  };
+
+  setLog(){
+    this.log.push(this.setTimeStamp());
+  }
+
+  displayDetails(){
+    this.setLog();
+    if (!this.showPwd){
+      this.showPwd=true;
+    }else{
+      this.showPwd=false
+    }
+  };
+}
