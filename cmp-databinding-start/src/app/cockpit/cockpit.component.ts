748e315f7b83a1d8d95ff42c797d272f5b4f75e4
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-02-03 15:15:43 +0100

    Sezione 5, lezione 60. Splitting Apps into Components

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -12,4 +12,20 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
