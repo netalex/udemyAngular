00e89b3c7f005439b28c9535e1d222623aa7f5c3
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-02-03 16:04:24 +0100

    Sezione 5, lezione 62. Binding to Custom Properties

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type:'server', name:'testServer', content:'Just a test!'}];
   newServerName = '';
   newServerContent = '';
 
