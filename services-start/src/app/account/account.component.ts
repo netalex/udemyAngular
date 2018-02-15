2cd4682d2b82abbd1faffe405418ec6030cfb375
Alessandro Aprile <aprile.alessandro@gmail.com>
2018-02-15 04:20:22 +0100

    Sezione 9, lezione 02. Injecting Services into Services.

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import {AccountService} from "../account.service";
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LoggingService]
+  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
@@ -18,6 +18,6 @@ export class AccountComponent {
 
   onSetTo(status:string) {
     this.loggingService.logStatusChange(status);
-    this.accountService.updateStatus(this.id,status);
+    // this.accountService.updateStatus(this.id,status);
   }
 }
